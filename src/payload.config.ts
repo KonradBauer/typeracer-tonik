df527a25ec2107a09b65ccd79d0c5ecd6c2929b6
Konrad Bauer <konrad.bauer@remoterebels.com>
2026-02-25 19:20:03 +0100

    refactor(structure): establish feature-based folder structure

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -5,8 +5,8 @@ import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
 
-import { Users } from './collections/Users'
-import { Media } from './collections/Media'
+import { Users } from './infrastructure/payload/collections/Users'
+import { Media } from './infrastructure/payload/collections/Media'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
